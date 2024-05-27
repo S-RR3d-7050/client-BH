c39a8e254597583f206bd04630a16ec710f4e3e9
S-RR3d-7050 <dahmanishadi@fsb.u-carthage.tn>
2024-05-27 21:15:04 +0100

    Fix assessment

--- src/pages/evaluation/student/ViewAssessment.jsx
+++ src/pages/evaluation/student/ViewAssessment.jsx
@@ -1,27 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import styles from '../coordinator/styles.module.css';
 import Navbar from '../../../components/ELEMENTS/Nav/Navbar';
-import SupeSidebar from '../../../components/ELEMENTS/Nav/SupeSidebar';
+import SideBar from '../../../components/ELEMENTS/Nav/SideBar';
+import Footer from '../../../components/ELEMENTS/Nav/Footer';
 import { useTranslation } from 'react-i18next';
 import Header from '../../../components/ELEMENTS/Header/Header';
 import HeaderTwo from '../../../components/ELEMENTS/Header/HeaderTwo';
-// import { FaCheck } from 'react-icons/fa'; // Uncomment this line if you use react-icons
-//import { useParams } from 'react-router-dom'
-import SideBar from '../../../components/ELEMENTS/Nav/SideBar';
-import Footer from '../../../components/ELEMENTS/Nav/Footer';
-
 
 const ViewAssessment = () => {
-    //const { id } = useParams();
-
     let user = localStorage.getItem('user');
-    //console.log(user);
     user = JSON.parse(user);
-    //console.log(user.id);
     const id = user.id;
 
-    const [noEval , setNoEval] = useState(true);
-    const [t, i18n] = useTranslation('global');
+    const [noEval, setNoEval] = useState(true);
     const [evaluation, setEvaluation] = useState({
         qualityOfStudentInternshipReport: '',
         experienceGained: '',
@@ -30,24 +21,23 @@ const ViewAssessment = () => {
         overall: '',
         comments: ''
     });
+    const [t, i18n] = useTranslation('global');
 
     useEffect(() => {
-        // Fetch the evaluation data from the backend
         const fetchEvaluation = async () => {
             try {
-                const response = await fetch('http://localhost:5000/api/v1/evaluations/student/last/'+id); // Adjust the URL as necessary
+                const response = await fetch('http://localhost:5000/api/v1/evaluations/student/last/' + id);
                 const data = await response.json();
                 setEvaluation(data.message);
-                if(data.message === []){
-                    setNoEval(true);
-                }
+                setNoEval(data.message.length === 0);  // Update noEval based on whether data.message is an empty array
             } catch (error) {
                 console.error('Error fetching evaluation:', error);
+                setNoEval(true);
             }
         };
 
         fetchEvaluation();
-    }, []);
+    }, [id]);
 
     const evalBox = {
         display: 'flex',
@@ -62,11 +52,11 @@ const ViewAssessment = () => {
 
     const getCheckBoxContent = (value, criterion) => (
         <div style={evalBox}>
-            {value === criterion ? '✔' : null} {/* Replace '✔' with <FaCheck /> if using react-icons */}
+            {value === criterion ? '✔' : null}
         </div>
     );
 
-    const displayEvalFields = [
+    const displayEvalFields = evaluation && [
         { name: 'Quality of student internship report', value: evaluation.qualityOfStudentInternshipReport },
         { name: 'Experience gained', value: evaluation.experienceGained },
         { name: 'Presentation', value: evaluation.presentation },
@@ -87,56 +77,80 @@ const ViewAssessment = () => {
             <Navbar />
             <SideBar />
             <section className={styles.main}>
-                <Header 
+                <Header
                     text={t('eval.stud')}
                     color={'#003679'}
                     fontSize={'22px'}
                     fontWeight={'600'}
                     margin={'1.5rem 1.5rem'}
                 />
-                <table>
-                    <thead>
-                        <tr>
-                            <th style={{width: '60%', textAlign: 'left', fontSize: '20px'}}>{t('eval.crit')}</th>
-                            <th>{t('eval.poor')}</th>
-                            <th>{t('eval.fair')}</th>
-                            <th>{t('eval.good')}</th>
-                            <th>{t('eval.exc')}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        noEval ? <tr><td colSpan="5">No evaluation found</td></tr> : null
-                    }
-                        {displayEvalFields}
-                    </tbody>
-                </table>
-                <section className={styles.summNcomm}>
-                    <div className={styles.summary}>
-                        <HeaderTwo 
-                            text={t('eval.summary') + ':'}
-                            color={'#003679'}
-                            fontSize={'20px'}
-                            fontWeight={'600'}
-                            margin={'1rem 1rem'}
-                        />
-                        <div className={styles.summaryCont}>
-                            Summary goes in here!
-                        </div>
-                    </div>
-                    <div className={styles.summary}>
-                        <HeaderTwo 
-                            text={t('eval.comm') + ':'}
-                            color={'#003679'}
-                            fontSize={'20px'}
-                            fontWeight={'600'}
-                            margin={'1rem 1rem'}
-                        />
-                        <div className={styles.summaryCont}>
-                            {evaluation.comments}
-                        </div>
-                    </div>
-                </section>
+                {noEval ? (
+                    <>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th style={{ width: '60%', textAlign: 'left', fontSize: '20px' }}>{t('eval.crit')}</th>
+                                    <th>{t('eval.poor')}</th>
+                                    <th>{t('eval.fair')}</th>
+                                    <th>{t('eval.good')}</th>
+                                    <th>{t('eval.exc')}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><p className="noEvalMessage">No evaluation found</p></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+
+                        </table>
+                    </>                ) : (
+                    <>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th style={{ width: '60%', textAlign: 'left', fontSize: '20px' }}>{t('eval.crit')}</th>
+                                    <th>{t('eval.poor')}</th>
+                                    <th>{t('eval.fair')}</th>
+                                    <th>{t('eval.good')}</th>
+                                    <th>{t('eval.exc')}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {displayEvalFields}
+                            </tbody>
+                        </table>
+                        <section className={styles.summNcomm}>
+                            <div className={styles.summary}>
+                                <HeaderTwo
+                                    text={t('eval.summary') + ':'}
+                                    color={'#003679'}
+                                    fontSize={'20px'}
+                                    fontWeight={'600'}
+                                    margin={'1rem 1rem'}
+                                />
+                                <div className={styles.summaryCont}>
+                                    Summary goes in here!
+                                </div>
+                            </div>
+                            <div className={styles.summary}>
+                                <HeaderTwo
+                                    text={t('eval.comm') + ':'}
+                                    color={'#003679'}
+                                    fontSize={'20px'}
+                                    fontWeight={'600'}
+                                    margin={'1rem 1rem'}
+                                />
+                                <div className={styles.summaryCont}>
+                                    {evaluation.comments}
+                                </div>
+                            </div>
+                        </section>
+                    </>
+                )}
             </section>
             <Footer />
         </>
